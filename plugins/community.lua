b3cb80c727b208cac7a43c7fc9d2e18ea2dce8d3
Camilo Garcia <camilo_7@msn.com>
2023-06-14 09:23:24 +0200

    add surround and other fixes

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,6 +5,15 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.everforest" },
+  {
+    "everforest",
+    config = function()
+      vim.g.everforest_transparent_background = 2
+      vim.g.everforest_background = "hard"
+    end,
+  },
+  -- { import = "astrocommunity.colorscheme.kanagawa" },
   { import = "astrocommunity.markdown-and-latex.vimtex" },
   {
     "lervag/vimtex",
@@ -17,5 +26,6 @@ return {
       }
     end,
   },
+  { import = "astrocommunity.motion.nvim-surround" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
